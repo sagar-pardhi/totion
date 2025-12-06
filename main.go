7ae8b4a135a5b09964b52754834669e91b6ce778
sagar-pardhi <sgrpardhi007@gmail.com>
2025-12-06 15:21:33 +0000

    add create new file feature

--- main.go
+++ main.go
@@ -2,19 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
+var (
+	vaultDir string
+)
+
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Error getting home directory", err)
+	}
+	vaultDir = fmt.Sprintf("%s/.totion", homeDir)
+}
+
 type model struct {
-	msg string
+	newFileInput           textinput.Model
+	createFileInputVisible bool
+	currentFile            *os.File
+	noteTextArea           textarea.Model
 }
 
 func initialModel() model {
+	err := os.MkdirAll(vaultDir, 0750)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ti := textinput.New()
+	ti.Placeholder = "Enter file name"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 50
+	ti.PromptStyle = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{})
+	ti.TextStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#fff"))
+	ti.PlaceholderStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#fff"))
+
+	ta := textarea.New()
+	ta.Placeholder = "Write your note here..."
+	ta.Focus()
+	ta.ShowLineNumbers = false
+
 	return model{
-		msg: "Hello, World",
+		newFileInput:           ti,
+		createFileInputVisible: false,
+		noteTextArea:           ta,
 	}
 }
 
@@ -23,6 +63,7 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
 	switch msg := msg.(type) {
 
 	case tea.KeyMsg:
@@ -31,9 +72,78 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 
+		case "ctrl+n":
+			m.createFileInputVisible = true
+			return m, nil
+
+		case "ctrl+s":
+			if m.currentFile == nil {
+				break
+			}
+
+			if err := m.currentFile.Truncate(0); err != nil {
+				fmt.Println("cannot save the file 🥲")
+				return m, nil
+			}
+
+			if _, err := m.currentFile.Seek(0, 0); err != nil {
+				fmt.Println("cannot save the file 🥲")
+				return m, nil
+			}
+
+			if _, err := m.currentFile.WriteString(m.noteTextArea.Value()); err != nil {
+				fmt.Println("cannot save the file 🥲")
+				return m, nil
+			}
+
+			if err := m.currentFile.Close(); err != nil {
+				fmt.Println("cannot close the file")
+			}
+
+			m.currentFile = nil
+			m.noteTextArea.SetValue("")
+
+			return m, nil
+
+		case "enter":
+			if m.currentFile != nil {
+				break
+			}
+
+			filename := m.newFileInput.Value()
+
+			if filename != "" {
+				filepath := fmt.Sprintf("%s/%s.md", vaultDir, filename)
+
+				if _, err := os.Stat(filepath); err == nil {
+					return m, nil
+				}
+
+				file, err := os.Create(filepath)
+
+				if err != nil {
+					log.Fatalf("%v", err)
+				}
+
+				m.currentFile = file
+				m.createFileInputVisible = false
+				m.newFileInput.SetValue("")
+			}
+
+			return m, nil
 		}
+
 	}
-	return m, nil
+
+	if m.createFileInputVisible {
+		m.newFileInput, cmd = m.newFileInput.Update(msg)
+	}
+
+	if m.currentFile != nil {
+		m.noteTextArea, cmd = m.noteTextArea.Update(msg)
+	}
+
+	return m, cmd
 }
 
 func (m model) View() string {
@@ -50,6 +160,14 @@ func (m model) View() string {
 	help := "Ctrl+N: new file . Ctrl+L: List . Esc: back/save . Ctrl+S: Save . Ctrl+Q: quit"
 
 	view := ""
+
+	if m.createFileInputVisible {
+		view = m.newFileInput.View()
+	}
+
+	if m.currentFile != nil {
+		view = m.noteTextArea.View()
+	}
 
 	return fmt.Sprintf("\n%s\n\n%s\n\n%s", welcome, view, help)
 }
